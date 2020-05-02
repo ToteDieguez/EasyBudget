440e6e49f9703bcd26e9ed0bfb8e68547a9a34e3
ToteDieguez <totedieguez@gmail.com>
2020-05-02 14:42:33 +0100

    BackEnd Config project

---
+++ BackEnd/EasyBudget/src/main/java/com/easybudget/EasyBudget/EasyBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.easybudget.EasyBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyBudgetApplication.class, args);
+	}
+
+}
